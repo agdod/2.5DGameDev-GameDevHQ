58b3d1817c2e73bf9a3b46e8922c912316eab1d7
agdodson <andy.dodson@outlook.com>
2021-02-26 15:17:51 +0200

    - Pressure pad tweaks.
    -- Changed distance to center.
    -- Changed color of pressure pad when active.
    -- Remove rigidbody from pushed object, when pressure pad is activated.

--- Assets/Scripts/PressurePad.cs
+++ Assets/Scripts/PressurePad.cs
@@ -14,15 +14,23 @@ public class PressurePad : MonoBehaviour
 		{
 			// check when close to center
 			float distance = Vector3.Distance(transform.position, other.transform.position);
-			if (distance < 0.05)
+			if (distance < 0.5)
 			{
 				other.attachedRigidbody.isKinematic = true;
 				if (other.TryGetComponent(out MeshRenderer mesh))
 				{
 					mesh.material.color = Color.blue;
 				}
-
+				// Remove the rigidBody componet to stop ontrigger being called.
+				Destroy(other.attachedRigidbody);
+				// Change color of pad to red
+				MeshRenderer thisMesh = GetComponentInChildren<MeshRenderer>();
+				if (thisMesh != null)
+				{
+					thisMesh.material.color = Color.red;
+				}
 			}
+
 
 		}
 	}
